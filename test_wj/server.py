b75905366ca0b1c3cfac1dc6240582f8211108a5
mi-ci <73093748+mi-ci@users.noreply.github.com>
2024-04-24 16:39:57 +0900

    Update server.py

--- test_wj/server.py
+++ test_wj/server.py
@@ -3,7 +3,7 @@ import pyrebase
 config = {
     "apiKey": "AIzaSyAEiuF044Rb24Lhkh8xwYZf6MFlDkIleNk",
     "authDomain": "suhwa-mbc.firebaseapp.com",
-    "databaseURL": "https://console.firebase.google.com/u/0/project/suhwa-mbc/database/suhwa-mbc-default-rtdb/data/~2F",
+    "databaseURL": "https://suhwa-mbc-default-rtdb.asia-southeast1.firebasedatabase.app",
     "storageBucket": "suhwa-mbc.appspot.com"
 }
 
