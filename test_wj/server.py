5c4e0ec3eb594fa70acb5bb4c39ec37a3d2ed687
mi-ci <73093748+mi-ci@users.noreply.github.com>
2024-04-24 16:38:51 +0900

    Update server.py

--- test_wj/server.py
+++ test_wj/server.py
@@ -2,9 +2,9 @@ import pyrebase
 
 config = {
     "apiKey": "AIzaSyAEiuF044Rb24Lhkh8xwYZf6MFlDkIleNk",
-    "authDomain": "project-id.firebaseapp.com",
-    "databaseURL": "https://project-id.firebaseio.com",
-    "storageBucket": "project-id.appspot.com"
+    "authDomain": "suhwa-mbc.firebaseapp.com",
+    "databaseURL": "https://console.firebase.google.com/u/0/project/suhwa-mbc/database/suhwa-mbc-default-rtdb/data/~2F",
+    "storageBucket": "suhwa-mbc.appspot.com"
 }
 
 firebase = pyrebase.initialize_app(config)
